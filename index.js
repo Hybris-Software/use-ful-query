cc755be00aa5df9e45ad4b5ef0861d2d707ff40f
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2022-09-16 10:44:43 +0400

    added packages for build npm

--- index.js
+++
@@ -1,7 +0,0 @@
-import useQuery from "./Hooks/useQuery";
-import generateApiClient from "./Api/client";
-import ApiProvider from "./Components/ApiProvider";
-
-export default useQuery;
-
-export { generateApiClient, ApiProvider };
